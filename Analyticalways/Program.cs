4b3f9416621522755663b40af20099169ceba74e
sairio peña <sairio20@gmail.com>
2020-08-18 06:58:54 -0400

    Add project files.

---
+++ Analyticalways/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Analyticalways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
